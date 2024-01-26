8a07cd2b0f7dd82e5d2be9eeb5174077fd74ac9b
Olha-Suchok <suchok_olya@ukr.net>
2024-01-26 18:31:09 +0200

    Аdded block of images to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,100 @@
 import RatingBlock from "components/common/RatingBlock/RatingBlock";
-import { Container, Title, Text } from "./Footer.styled";
+
+import firstImageDesctop1 from "../../images/pictures/footerSection/1image1x@-1464.jpg";
+import firstImageDesctop2 from "../../images/pictures/footerSection/1image2x@-1464.jpg";
+
+import firstImageMobile1 from "../../images/pictures/footerSection/1image1x@-428.jpg";
+import firstImageMobile2 from "../../images/pictures/footerSection/1image2x@-428.jpg";
+
+import secondImageDesctop1 from "../../images/pictures/footerSection/2image1x@-1464.jpg";
+import secondImageDesctop2 from "../../images/pictures/footerSection/2image2x@-1464.jpg";
+
+import secondImageMobile1 from "../../images/pictures/footerSection/2image1x@-428.jpg";
+import secondImageMobile2 from "../../images/pictures/footerSection/2image2x@-428.jpg";
+
+import thirdImageDesctop1 from "../../images/pictures/footerSection/3image1x@-1464.jpg";
+import thirdImageDesctop2 from "../../images/pictures/footerSection/3image2x@-1464.jpg";
+
+import thirdImageMobile1 from "../../images/pictures/footerSection/3image1x@-428.jpg";
+import thirdImageMobile2 from "../../images/pictures/footerSection/3image2x@-428.jpg";
+
+import {
+  Container,
+  Title,
+  Text,
+  ImagesGroup,
+  FirstImageWrapper,
+  SecondImageWrapper,
+  ThirdImageWrapper,
+  FirstTransparentBlock,
+  SecondTransparentBlock,
+  SideImage,
+  CenterImage,
+} from "./Footer.styled";
 
 export default function Footer() {
   return (
     <Container>
       <Title>Find something you love.</Title>
       <Text>Click below to browse our collection!</Text>
-      <p>SLIDER</p>
+      <ImagesGroup>
+        <FirstImageWrapper>
+          <source
+            srcSet={`${thirdImageDesctop1} 1x, ${thirdImageDesctop2} 2x`}
+            media="(min-width:1464px)"
+          />
+          <source
+            srcSet={`${thirdImageMobile1} 1x, ${thirdImageMobile2} 2x`}
+            media="(min-width:428px)"
+          />
+          <SideImage
+            src={thirdImageMobile2}
+            loading="lazy"
+            alt="Girl"
+            // width={"100px"}
+            // height={"150px"}
+          ></SideImage>
+        </FirstImageWrapper>
+
+        <SecondImageWrapper>
+          <source
+            srcSet={`${secondImageDesctop1} 1x, ${secondImageDesctop2} 2x`}
+            media="(min-width:1464px)"
+          />
+          <source
+            srcSet={`${secondImageMobile1} 1x, ${secondImageMobile2} 2x`}
+            media="(min-width:428px)"
+          />
+          <CenterImage
+            src={secondImageMobile2}
+            loading="lazy"
+            alt="Girl"
+            // width={"140px"}
+            // height={"211px"}
+          ></CenterImage>
+        </SecondImageWrapper>
+
+        <ThirdImageWrapper>
+          <source
+            srcSet={`${firstImageDesctop1} 1x, ${firstImageDesctop2} 2x`}
+            media="(min-width:1464px)"
+          />
+          <source
+            srcSet={`${firstImageMobile1} 1x, ${firstImageMobile2} 2x`}
+            media="(min-width:428px)"
+          />
+          <SideImage
+            src={firstImageMobile2}
+            loading="lazy"
+            alt="Girl"
+            // width={"100px"}
+            // height={"150px"}
+          ></SideImage>
+        </ThirdImageWrapper>
+
+        <FirstTransparentBlock></FirstTransparentBlock>
+        <SecondTransparentBlock></SecondTransparentBlock>
+      </ImagesGroup>
       <RatingBlock test={"Over 500+ 5 Star Reviews Online"} />
     </Container>
   );
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -12,6 +12,9 @@ export const Container = styled.footer`
   margin-left: auto;
   margin-right: auto;
 
+  background: #f9f0e5;
+  background: linear-gradient(to top, #f9f0e5 0%, #ffffff 90%);
+
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: ${({ theme }) => theme.breakpoints.mobile};
   }
@@ -47,5 +50,114 @@ export const Text = styled.p`
   color: ${({ theme }) => theme.colors.MAIN_TEXT_COLOR};
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+  }
+`;
+
+export const ImagesGroup = styled.div`
+  position: relative;
+
+  width: 400px;
+  height: 270px;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 815px;
+    height: 460px;
+  }
+`;
+
+export const FirstImageWrapper = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 41px;
+
+  z-index: 2;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    right: 94px;
+  }
+`;
+
+export const SecondImageWrapper = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 129px;
+
+  z-index: 3;
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    left: 280px;
+  }
+`;
+
+export const ThirdImageWrapper = styled.div`
+  position: absolute;
+  top: 50px;
+  left: 30px;
+
+  z-index: 2;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    left: 67px;
+  }
+`;
+
+export const FirstTransparentBlock = styled.div`
+  position: absolute;
+  bottom: 110px;
+  left: 5px;
+
+  width: 67px;
+  height: 95px;
+
+  background: #f9f0e5;
+  background: linear-gradient(to top, #f9f0e5 0%, #ffffff 90%);
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 139px;
+    height: 196px;
+    background: linear-gradient(to top, #f9f0e5 29%, #ffffff 100%);
+
+    bottom: 140px;
+    left: 10px;
+  }
+`;
+
+export const SecondTransparentBlock = styled.div`
+  position: absolute;
+  bottom: 110px;
+  right: 5px;
+
+  width: 67px;
+  height: 95px;
+
+  background: #f9f0e5;
+  background: linear-gradient(to top, #f9f0e5 0%, #ffffff 90%);
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 139px;
+    height: 196px;
+    background: linear-gradient(to top, #f9f0e5 29%, #ffffff 100%);
+
+    bottom: 140px;
+    right: 30px;
+  }
+`;
+
+export const SideImage = styled.img`
+  width: 97px;
+  height: 145px;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 210px;
+    height: 316px;
+  }
+`;
+
+export const CenterImage = styled.img`
+  width: 140px;
+  height: 210px;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 246px;
+    height: 373px;
   }
 `;

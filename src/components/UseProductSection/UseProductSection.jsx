a65d38c97c6c3e3eb0d4a199a23b712f31e3079e
Olha-Suchok <suchok_olya@ukr.net>
2024-01-27 12:21:32 +0200

    Added a slider to the UseProduct section on mobile version

--- src/components/UseProductSection/UseProductSection.jsx
+++ src/components/UseProductSection/UseProductSection.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import RatingBlock from "components/common/RatingBlock/RatingBlock";
 
 import Basket from "images/useProductIcons/Basket/Basket";
 import Car from "images/useProductIcons/Car/Car";
 import Sun from "images/useProductIcons/Sun/Sun";
+import ArrowLeft from "images/contentIcons/ArrowLeft/ArrowLeft";
+import ArrowRight from "images/contentIcons/ArrowRight/ArrowRight";
 
 import {
   Section,
@@ -12,9 +16,44 @@ import {
   CardListItemIcon,
   CardListItemMainText,
   CardListItemText,
+  CardListMobile,
+  CardListItemMobile,
+  CardListItemIconMobile,
+  CardListItemMainTextMobile,
+  CardListItemTextMobile,
+  SliderWrapperMobile,
+  ArrowLeftButtonMobile,
+  ArrowRightButtonMobile,
 } from "./UseProductSection.styled";
 
 export default function UseProductSection() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const data = [
+    {
+      icon: <Basket />,
+      title: "You save.",
+      text: "Browse our comfort sets and save 15% when you bundle.",
+    },
+    {
+      icon: <Car />,
+      title: "We ship.",
+      text: "We ship your items within 1-2 days of receiving your order.",
+    },
+    {
+      icon: <Sun />,
+      title: "You enjoy!",
+      text: "Wear hernest around the house, out on the town, or in bed.",
+    },
+  ];
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+  };
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
+  };
+
   return (
     <Section>
       <Title>Comfort made easy</Title>
@@ -49,6 +88,31 @@ export default function UseProductSection() {
           </CardListItemText>
         </CardListItem>
       </CardList>
+
+      <SliderWrapperMobile>
+        <ArrowLeftButtonMobile type="button" onClick={handlePrevClick}>
+          <ArrowLeft />
+        </ArrowLeftButtonMobile>
+
+        <CardListMobile>
+          <CardListItemMobile>
+            <CardListItemIconMobile>
+              {data[currentIndex].icon}
+            </CardListItemIconMobile>
+            <CardListItemMainTextMobile>
+              {data[currentIndex].name}
+            </CardListItemMainTextMobile>
+            <CardListItemTextMobile>
+              {data[currentIndex].text}
+            </CardListItemTextMobile>
+          </CardListItemMobile>
+        </CardListMobile>
+
+        <ArrowRightButtonMobile type="button" onClick={handleNextClick}>
+          <ArrowRight />
+        </ArrowRightButtonMobile>
+      </SliderWrapperMobile>
+
       <RatingBlock test={"Over 500+ 5 Star Reviews Online"} />
     </Section>
   );
--- src/components/UseProductSection/UseProductSection.styled.js
+++ src/components/UseProductSection/UseProductSection.styled.js
@@ -26,6 +26,7 @@ export const Title = styled.h2`
   text-align: center;
 
   margin-top: 0;
+  margin-bottom: 50px;
 
   font-size: 26px;
   font-family: ${({ theme }) => theme.fonts.sofiaProRegular};
@@ -40,22 +41,18 @@ export const Title = styled.h2`
 `;
 
 export const CardList = styled.ul`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-
-  margin-bottom: 30px;
-  margin-top: 0;
-  padding-left: 0;
-
+  display: none;
   list-style: none;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    display: flex;
     flex-direction: row;
+    justify-content: center;
     align-items: center;
 
     margin-bottom: 56px;
+    margin-top: 0;
+    padding-left: 0;
   }
 `;
 
@@ -120,4 +117,115 @@ export const CardListItemText = styled.p`
   line-height: 1.5;
 
   color: #676869;
+`;
+
+export const CardListMobile = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  margin-bottom: 0;
+  margin-top: 0;
+  padding-left: 0;
+
+  list-style: none;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    display: none;
+  }
+`;
+
+export const CardListItemMobile = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 288px;
+  height: 288px;
+
+  padding: 30px;
+  margin-right: 0;
+
+  border-radius: 8px;
+  background-color: #ededed;
+
+  &:nth-of-type(2n) {
+    background-color: #f9f0e6;
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 20px;
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    &:not(:last-child) {
+      margin-right: 40px;
+    }
+
+    margin-bottom: 0;
+    &:not(:last-child) {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export const CardListItemIconMobile = styled.div`
+  margin-bottom: 0;
+`;
+
+export const CardListItemMainTextMobile = styled.p`
+  margin-top: 0;
+  margin-bottom: 15px;
+
+  font-size: 22px;
+  font-family: ${({ theme }) => theme.fonts.sofiaProRegular};
+  line-height: 1.9;
+
+  color: #01005b;
+`;
+
+export const CardListItemTextMobile = styled.p`
+  text-align: center;
+
+  margin-top: 0;
+  margin-bottom: 0;
+
+  font-size: 15px;
+  font-family: ${({ theme }) => theme.fonts.sofiaProRegular};
+  line-height: 1.5;
+
+  color: #676869;
+`;
+
+export const ArrowLeftButtonMobile = styled.button`
+  margin-right: 20px;
+
+  border: none;
+  background-color: transparent;
+
+  cursor: pointer;
+`;
+
+export const ArrowRightButtonMobile = styled.button`
+  margin-left: 20px;
+  border: none;
+  background-color: transparent;
+
+  cursor: pointer;
+`;
+
+export const SliderWrapperMobile = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  padding-bottom: 0;
+  margin-bottom: 40px;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    display: none;
+  }
 `;
